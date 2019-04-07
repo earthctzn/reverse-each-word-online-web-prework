8dd27fab480583e392f6eabe665c2aa1f3b8bd09
Caleb Einarr <earthctzn1@gmail.com>
2019-04-07 21:16:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,4 +7,15 @@ def reverse_each_word(sentence)
   end
   new_string = new_array.join(" ")
 
+end
+
+def reverse_each_word(phrase)
+  array= phrase.split
+  new_array =[]
+  array.each do |word|
+    word.reverse
+    new_array << word.reverse
+  end
+  new_string = new_array.join(" ")
+
 end  
