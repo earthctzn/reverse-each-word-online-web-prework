5cda17e5496c526f9f43a12455fc7c56be51da20
Caleb Einarr <earthctzn1@gmail.com>
2019-04-07 20:54:42 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,7 @@
-def reverse_each_word(str)
-    str.reverse!
+def reverse_string(string)
+  split_string = string.split("")
+  reversed = []
+  string.size.times { reversed << split_string.pop }
+  reversed.join
 end
+puts reverse_string("hello")
